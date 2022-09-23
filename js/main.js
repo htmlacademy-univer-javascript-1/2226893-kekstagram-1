37bd14542449383dd69bae8ac2333b98c39adc93
KlarkGames <laletin.alesha@mail.ru>
2022-09-23 17:13:27 +0300

    main.js connected; add supportive functions

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function getRandom (min, max) {
+  if (max < min) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  return Math.floor(Math.random() * (Math.abs(max - min) + 1)) + min;
+}
+
+const checkLength = (str,  maxLength) => str.length <= maxLength;
+
