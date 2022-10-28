1fe5cb085e641dc2ba96c5a45d82f192a7204e51
KlarkGames <laletin.alesha@mail.ru>
2022-10-28 13:19:27 +0300

    fix 1

--- js/picture.js
+++ js/picture.js
@@ -1,14 +1,13 @@
 import {createPhotoDescriptions} from'./data.js';
 
 const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
-console.log(templatePicture);
 
 const fragment = document.createDocumentFragment();
 
-let photoDescriptions = createPhotoDescriptions();
+const photoDescriptions = createPhotoDescriptions();
 
 for (let i = 0; i < photoDescriptions.length; i++) {
-  let photoElement = templatePicture.cloneNode(true);
+  const photoElement = templatePicture.cloneNode(true);
   photoElement.querySelector('img').src = photoDescriptions[i].url;
   photoElement.querySelector('.picture__likes').textContent = photoDescriptions[i].likes;
   photoElement.querySelector('.picture__comments').textContent = photoDescriptions[i].comments.length;
