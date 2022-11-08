6f19af4db7384880fb3635cb7985642f03b34c91
KlarkGames <laletin.alesha@mail.ru>
2022-11-08 14:26:43 +0300

    fix displaying of comments

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,5 +1,22 @@
 const bigPicture = document.querySelector('.big-picture');
 
+const commentArea = bigPicture.querySelector('.social__comments');
+commentArea.removeChild(commentArea.querySelector('li'));
+commentArea.removeChild(commentArea.querySelector('li'));
+/*
+4.6. Все комментарии к изображению выводятся в блок .social__comments.
+    Сразу после открытия изображения в полноэкранном режиме отображается не более 5 комментариев. Количество показанных комментариев и общее число
+    комментариев отображается в блоке .social__comment-count.
+    Пример разметки списка комментариев приведён в блоке .social__comments.
+    Комментарий оформляется отдельным элементом списка li с классом social__comment.
+    Аватарка автора комментария отображается в блоке .social__picture. Имя автора комментария отображается в атрибуте alt его аватарки.
+    Текст комментария выводится в блоке .social__text.
+
+4.7. Отображение дополнительных комментариев происходит при нажатии на кнопку .comments-loader.
+    При нажатии на кнопку отображается не более 5 новых комментариев.
+    При изменении количества показанных комментариев число показанных комментариев в блоке .social__comment-count также изменяется.
+*/
+
 const createCommentBlock = function (comment) {
   const item = document.createElement('li');
   item.classList.add('social__comment');
@@ -20,6 +37,23 @@ const createCommentBlock = function (comment) {
   return item;
 };
 
+const createCommentsList = function (comments) {
+  if (comments.length !== 0) {
+    for (let i = 0; i < comments.length; i++) {
+      commentArea.append(createCommentBlock(comments[i]));
+    }
+  }
+};
+
+const removeCommentsList = function () {
+  const children = commentArea.querySelectorAll('li');
+  if (children.length > 0) {
+    for (let i = children.length - 1 ; i >= 0; i--) {
+      commentArea.removeChild(children[i]);
+    }
+  }
+}
+
 const showBigPicture = function (element, picture) {
   element.addEventListener('click', () => {
     document.body.classList.add('modal-open');
@@ -29,14 +63,8 @@ const showBigPicture = function (element, picture) {
     bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
     bigPicture.querySelector('.social__caption').textContent = picture.description;
 
-    const commentArea = bigPicture.querySelector('.social__comments');
-    if (picture.comments.length !== 0) {
-      for (let i = 0; i < picture.comments.length; i++) {
-        commentArea.append(createCommentBlock(picture.comments[i]));
-      }
-    }
-    bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-    bigPicture.querySelector('.comments-loader').classList.add('hidden');
+    createCommentsList(picture.comments);
+
     bigPicture.classList.remove('hidden');
   });
 };
@@ -45,12 +73,14 @@ const button = bigPicture.querySelector('.cancel');
 button.addEventListener('click', () => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  removeCommentsList();
 });
 
 document.addEventListener('keydown', (evt) => {
   if (evt.code === 'Escape') {
     bigPicture.classList.add('hidden');
     document.body.classList.remove('modal-open');
+    removeCommentsList();
   }
 });
 
