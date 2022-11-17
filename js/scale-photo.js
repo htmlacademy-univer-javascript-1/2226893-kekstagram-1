67d3b98d9d419c2a1be55ab80f703dbbd65cc649
KlarkGames <laletin.alesha@mail.ru>
2022-11-17 22:08:46 +0300

    fix 1

--- js/scale-photo.js
+++ js/scale-photo.js
@@ -3,7 +3,7 @@ const SCALE_STEP = 25;
 const preview = document.querySelector('.img-upload__preview');
 
 const onSmallerButton = function () {
-  let value = Number(scaleValue.value.substring(0, scaleValue.value.length - 1));
+  const value = Number(scaleValue.value.substring(0, scaleValue.value.length - 1));
   if (value > 25) {
     scaleValue.value = `${value - SCALE_STEP}%`;
     preview.style.transform = `scale(${(value - SCALE_STEP) / 100})`;
@@ -12,7 +12,7 @@ const onSmallerButton = function () {
 };
 
 const onBiggerButton = function () {
-  let value = Number(scaleValue.value.substring(0, scaleValue.value.length - 1));
+  const value = Number(scaleValue.value.substring(0, scaleValue.value.length - 1));
   if (value < 100) {
     scaleValue.value = `${value + SCALE_STEP}%`;
     preview.style.transform = `scale(${(value + SCALE_STEP) / 100})`;
