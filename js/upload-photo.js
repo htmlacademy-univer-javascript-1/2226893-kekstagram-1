3109762c22225d08f45b3da922c64ab111b63728
KlarkGames <laletin.alesha@mail.ru>
2022-12-17 17:50:36 +0300

    add upload photo

---
+++ js/upload-photo.js
@@ -0,0 +1,17 @@
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+const fileChooser = document.querySelector('#upload-file');
+const preview = document.querySelector('.img-upload__preview').querySelector('img');
+
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => {
+    return fileName.endsWith(it);
+  });
+
+  if (matches) {
+    preview.src = URL.createObjectURL(file);
+  }
+});
